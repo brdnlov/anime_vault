22350dab6a33d74c1c6c7de4f821f5346899b619
Brandon Lov <1LovBr2012@gmail.com>
2024-11-13 23:19:51 -0800

    updates

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -13,7 +13,7 @@ function Hero() {
         />
         <h1 className="sm:text-6xl text-5xl text-white lg:max-w-lg font-bold leading-[120%]">
           Explore The <span className="red-gradient">Diverse Realms</span> of
-          Anime Magic
+          Anime Series
         </h1>
       </div>
       <div className="lg:flex-1 relative w-full h-[50vh] justify-center">
